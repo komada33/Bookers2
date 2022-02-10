ee9b307495a2a3d9bab8bd60f608df332217f74c
EC2 Default User <ec2-user@ip-172-31-22-2.ec2.internal>
2022-02-10 13:08:53 +0000

    途中３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
+    @books = @user.books
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(id: current_user)
   end
 
   def index
     @users = User.all
   end
+
+  def  user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :index, :update]
   resources :books, only: [:new, :create, :index, :show]
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
