41f111207141c24dffc9c14fb966ea59d1444be0
EC2 Default User <ec2-user@ip-172-31-22-2.ec2.internal>
2022-02-24 11:48:33 +0000

    rspec

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,7 +36,6 @@ class BooksController < ApplicationController
   end
 
   def show
-
     @bookn = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  #before_action :sign_in_user, only: [:edit, :update]
+  #before_action :correct_user, only: [:edit, :update]
    def show
     @user = User.find(params[:id])
     @book = Book.new
@@ -6,16 +8,9 @@ class UsersController < ApplicationController
    end
 
   def edit
-    @user = current_user
-    if @user == current_user
-      render "edit"
-    else
-      redirect_to user_path(@user.id)
-    end
   end
 
   def update
-    @user = current_user
     if @user.update(user_params)
       redirect_to user_path(id: current_user), notice: 'You have updated user successfully.'
     else
@@ -32,6 +27,11 @@ class UsersController < ApplicationController
 
   def  user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(user_path(current_user.id)) unless @user == current_user
   end
 
 end
