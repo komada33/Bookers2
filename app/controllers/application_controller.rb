3d594f5efde8bd95d02c61785785c1ac592cc4b9
EC2 Default User <ec2-user@ip-172-31-22-2.ec2.internal>
2022-02-24 12:43:47 +0000

    rspec 修正完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,17 @@ class ApplicationController < ActionController::Base
    before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resources)
-    user_path(id: current_user.id)
+  def after_sign_in_path_for(resource)
+    user_path(resource)
   end
 
-  def after_sign_out_path_for(resources)
+  def after_sign_out_path_for(resource)
     root_path
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name],)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
